a9ff2ed51337125c03f1f4e3abe145209a4e4661
Kittirat <52350392+Kittirat@users.noreply.github.com>
2019-07-20 18:07:19 +0700

    Add files via upload

---
+++ Exercise4_Kittirat_P.py
@@ -0,0 +1,9 @@
+e=25
+g=30
+i=40
+c=20
+print("---Your Score---")
+print("Foundation English",e)
+print("General Business ",g)
+print("Introduction to Computer Systems",i)
+print("Computer Programming",c)
